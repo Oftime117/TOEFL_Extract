01ead0643948e3e8a0d7ceb00bf4bc7049ea6a1d
FTalour <florian.talour@u-psud.fr>
2016-11-20 13:06:35 +0100

    création du script pour les proportions de mots seuls

--- Extraction/script/ecartTypeProportionGroupe1.sh
+++ Extraction/script/ecartTypeProportionGroupe1.sh
@@ -4,25 +4,29 @@
 #input2=$2 # la liste des groupes de 1 à utiliser avec les occurences par texte
 #output=$3 # le nom du fichier de sortie
 
+if [ "$1" -eq "1" ]; then # supprimer tout
+	rm sommeMot.txt valeurs-mots-moyenneGroupe1.txt valeurs.txt types.txt mots-sum2-valeursGroupe1.txt proportion1MotListSansLangue.txt mots-sum2-valeursGroupe1_temp.txt 
+fi
+
 ########################
 # calcul de la moyenne #
 ########################
 
 # créaction des fichiers des moyennes
-if [ ! -f "sommeMot" ] ; then
+if [ ! -f "sommeMot.txt" ] ; then
 	sed -i '/^(/d' trainMotListLower.txt	# supprime la langue
-	sort trainMotListLower.txt | uniq -c | sort -bnr > sommeMot
-	sed -i 's/^\ \ *//' sommeMot	# supprime les multiples espaces
-	sed -i '0,/^\ /s///' sommeMot	# supprime l'espace en début de ligne
+	sort trainMotListLower.txt | uniq -c | sort -bnr > sommeMot.txt
+	sed -i 's/^\ \ *//' sommeMot.txt	# supprime les multiples espaces
+	sed -i '0,/^\ /s///' sommeMot.txt	# supprime l'espace en début de ligne
 	
-	cp sommeMot sommeMot_temp
-	./supprPonctu.sh sommeMot_temp sommeMot
-	rm sommeMot_temp
+	cp sommeMot.txt sommeMot_temp.txt
+	./supprPonctu.sh sommeMot_temp.txt sommeMot.txt
+	rm sommeMot_temp.txt
 fi
 
 # extraction pour contruire un tableau
-grep . sommeMot | cut -d" " -f 1 > valeurs
-grep . sommeMot | cut -d" " -f 2 > types
+grep . sommeMot.txt | cut -d" " -f 1 > valeurs.txt
+grep . sommeMot.txt | cut -d" " -f 2 > types.txt
 
 # creation tableau moyenne | valeur
 declare -A moyenne
@@ -31,13 +35,13 @@ declare -A valeurs
 SC=9 # on prend 9 chiffres après la virgule
 n=`cat trainMotList.txt | sed "/^$/d" | wc -l` # nombre total de mots dans trainMotList.txt
 
-if [ ! -f valeurs-mots-moyenneGroupe1.txt ] ; then
+if [ ! -f "valeurs-mots-moyenneGroupe1.txt" ] ; then
 	> valeurs-mots-moyenneGroupe1.txt
 
 	while read newline ;
 	do
 		valeur=`echo "${newline}" | cut -d" " -f 1`
-		if [ "$valeur" -ge "9900" ]; then 
+		if [ "$valeur" -gt "9900" ]; then 
 			type=`echo "${newline}" | cut -d" " -f 2`
 			if [ ! "$type" ] ; then
 				echo "pas de type pour la valeur: $valeur"
@@ -48,8 +52,8 @@ if [ ! -f valeurs-mots-moyenneGroupe1.txt ] ; then
 			echo "moyenne[$type] = $valeur / $n = ${moyenne["$type"]}"
 			echo "$valeur $type ${moyenne["$type"]}" >> valeurs-mots-moyenneGroupe1.txt
 		fi
-	done < sommeMot
-elif [ -f valeurs-mots-moyenneGroupe1.txt ] ; then
+	done < sommeMot.txt
+elif [ -f "valeurs-mots-moyenneGroupe1.txt" ] ; then
 	while read newline ;
 	do
 		valeur=`echo "${newline}" | cut -d" " -f 1`
@@ -71,19 +75,19 @@ declare -A sdev # ecart type
 
 if [ ! -f "mots-sum2-valeursGroupe1.txt" ] ; then
 
-	cp proportion1MotListLower.txt proportion1MotListSansLangue
+	cp proportion1MotListLower.txt proportion1MotListSansLangue.txt
 
-	sed -i '/^(/d' proportion1MotListSansLangue 			# supprime la langue
-	sed -i 's/^\ \ *//' proportion1MotListSansLangue 	# supprime les multiples espaces
-	sed -i '0,/^\ /s///' proportion1MotListSansLangue 	# supprime l'espace en début de ligne
-	sed -i '/^$/d' proportion1MotListSansLangue 			# supprime les lignes vides
+	sed -i '/^(/d' proportion1MotListSansLangue.txt 			# supprime la langue
+	sed -i 's/^\ \ *//' proportion1MotListSansLangue.txt 	# supprime les multiples espaces
+	sed -i '0,/^\ /s///' proportion1MotListSansLangue.txt 	# supprime l'espace en début de ligne
+	sed -i '/^$/d' proportion1MotListSansLangue.txt 			# supprime les lignes vides
 	
-	> proportion1MotListSansLangue_temp
+	> proportion1MotListSansLangue_temp.txt
 	
 	while read newline ;
 	do
 		type=`echo "${newline}" | cut -d" " -f 2`
-		grep " $type$" proportion1MotListSansLangue >> proportion1MotListSansLangue_temp
+		grep " $type$" proportion1MotListSansLangue.txt >> proportion1MotListSansLangue_temp.txt
 	done < valeurs-mots-moyenneGroupe1.txt
 
 	> mots-sum2-valeursGroupe1_temp.txt
@@ -115,7 +119,7 @@ if [ ! -f "mots-sum2-valeursGroupe1.txt" ] ; then
 			sum2["$type"]=$(echo "scale=$SC; ${sum2["$type"]} + (${moyenne["$type"]} - $valeur) * (${moyenne["$type"]} - $valeur)" | bc)
 			echo "$type ${sum2["$type"]} $valeur" >> mots-sum2-valeursGroupe1_temp.txt
 
-	done < proportion1MotListSansLangue_temp
+	done < proportion1MotListSansLangue_temp.txt
 
 	echo "Fin du calcul de la somme des carrés des différences"
 	
@@ -129,7 +133,7 @@ if [ ! -f "mots-sum2-valeursGroupe1.txt" ] ; then
 		echo "$type ${sum2["$type"]} ${valeurs["$type"]}" >> mots-sum2-valeursGroupe1.txt
 	done < valeurs-mots-moyenneGroupe1.txt
 	
-	# rm proportion1MotListSansLangue_temp
+	# rm proportion1MotListSansLangue_temp.txt
 elif [ -f "mots-sum2-valeursGroupe1.txt" ]; then
 
 ########################################
@@ -159,7 +163,7 @@ elif [ -f "mots-sum2-valeursGroupe1.txt" ]; then
 fi
 
 # fichiers de sortie
-> mots-sdevGroupe1
+> mots-sdevGroupe1.txt
 
 # calcul à partir de la somme des carrés des differences 
 echo "Lancement de la moyenne des ecarts et de la mise à la racine carré"
@@ -171,12 +175,12 @@ do
 	type=`echo "${newline}" | cut -d" " -f 2`
 	
 	# calcul de l'ecart type
-	sdev["$type"]=$(echo "scale=$SC; sqrt(${sum2["$type"]} / $n )" | bc) # racine carré de la moyenne
+	sdev["$type"]=$(echo "scale=$SC; sqrt(${sum2["$type"]} / ${valeurs["$type"]} )" | bc) # racine carré de la moyenne
 
-	echo "sqrt( sum2[$type] / n) = sqrt( ${sum2["$type"]} / $n ) = ${sdev["$type"]} < ${moyenne["$type"]}"
+	echo "sqrt( sum2[$type] / n) = sqrt( ${sum2["$type"]} / ${valeurs["$type"]} ) = ${sdev["$type"]} < ${moyenne["$type"]}"
 
 	# création du fichier à lire en c++
-	echo "$type ${sdev["$type"]}" >> mots-sdevGroupe1
+	echo "$type ${sdev["$type"]}" >> mots-sdevGroupe1.txt
 	
 done < valeurs-mots-moyenneGroupe1.txt
 
